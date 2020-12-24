2233e3c27de10f5845101701b6578528d79d174d
Ory Shvartz <oryshkat@gmail.com>
2020-12-24 17:45:46 +0200

    THings to add when I get home

---
+++ to-be-added/add-to-main.tf
@@ -0,0 +1,31 @@
+# at the bottom of main.tf append:
+
+provider "helm" {
+  version = "1.3.1"
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
+}
+
+resource "helm_release" "ingress" {
+  name       = "ingress"
+  chart      = "aws-alb-ingress-controller"
+  repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
+  version    = "1.0.2"
+
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
+  }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  set {
+    name  = "clusterName"
+    value = local.cluster_name
+  }
+}
