b03a892d0d267d95581bff410540a791557c5c1d
Ory Shvartz <oryshkat@gmail.com>
2020-12-26 20:27:26 +0200

    Removed the provider part of the alb ingress module that was causing issues

--- main.tf
+++ main.tf
@@ -210,10 +210,6 @@ module "alb_ingress_controller" {
   source  = "iplabs/alb-ingress-controller/kubernetes"
   version = "3.4.0"
 
-  /* providers = {
-    kubernetes = "kubernetes.eks"
-  } */
-
   k8s_cluster_type = "eks"
   k8s_namespace    = "kube-system"
 
